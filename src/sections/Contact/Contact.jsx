907622e212f9ca4aceeaf2de2bcb70c23a6755a3
devdele <ayodeletoluwani71@gmail.com>
2025-01-24 23:27:50 +0100

    New chnages

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,45 +1,15 @@
 import styles from './ContactStyles.module.css';
 
+
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
-      <h1 className="sectionTitle">Contact</h1>
       <form action="">
-        <div className="formGroup">
-          <label htmlFor="name" hidden>
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            required
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="email" hidden>
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="message" hidden>
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            required></textarea>
-        </div>
-        <input className="hover btn" type="submit" value="Submit" />
+    
+      <a href='https://wa.me/09073804271' target="_blank" rel="noopener noreferrer" type='submit'>
+        Contact
+      </a>
+
       </form>
     </section>
   );
