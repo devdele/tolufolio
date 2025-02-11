533edb9a4e3a6e80e7c60d5367563f0a7dcc0c3f
devdele <ayodeletoluwani71@gmail.com>
2025-02-11 21:34:44 +0100

    updated changes

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
     <section id="contact" className={styles.container}>
       <form action="">
     
-      <a href='https://wa.me/09073804271' target="_blank" rel="noopener noreferrer" type='submit'>
+      <a href='https://wa.me/09073804371' target="_blank" rel="noopener noreferrer" type='submit'>
         Contact
       </a>
 
