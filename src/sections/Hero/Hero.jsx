52c556313f540fc74ad7b07e401fff7dd577d05f
devdele <ayodeletoluwani71@gmail.com>
2025-05-13 18:03:01 +0100

    may changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -42,7 +42,7 @@ function Hero() {
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
+          <a href="https://x.com/Toluwani_Dev" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
           <a href="https://github.com/devdele" target="_blank">
